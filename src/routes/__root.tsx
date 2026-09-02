bfaca48ce372304ae82a4afea5800daefa9991c1
Wl. <zyrliuwei@gmail.com>
2026-09-02 17:03:58 +0800

    加入监控

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -123,6 +123,15 @@ function RootDocument({ children }: { children: ReactNode }) {
     <html lang={getLocale()} suppressHydrationWarning>
       <head>
         <HeadContent />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,"clarity","script","ybxqvxpkl1");`,
+          }}
+        />
       </head>
       <body className="font-sans antialiased">
         {children}
